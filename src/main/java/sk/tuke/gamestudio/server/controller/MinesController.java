e74d4f004b8705fe035d770dbfc4af0011637a14
Štefan Korečko <stefan.korecko@tuke.sk>
2022-09-21 10:53:36 +0200

    Webova verzia s plne funkcnym Minesweeper-om.

--- src/main/java/sk/tuke/gamestudio/server/controller/MinesController.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/MinesController.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
-import sk.tuke.gamestudio.game.mines.core.Clue;
-import sk.tuke.gamestudio.game.mines.core.Field;
-import sk.tuke.gamestudio.game.mines.core.Tile;
-import sk.tuke.gamestudio.game.mines.core.TileState;
+import sk.tuke.gamestudio.game.mines.core.*;
 
 import java.util.Date;
 
@@ -17,22 +14,50 @@ import java.util.Date;
 @Scope(WebApplicationContext.SCOPE_SESSION)
 public class MinesController {
 
+    private boolean marking=false;
+
     private Field mineField = new Field(9,9,3);
+
 
     @RequestMapping
     public String processUserInput(@RequestParam(required = false) Integer row,
                                    @RequestParam(required = false) Integer column){
 
         if(row!=null && column!=null){
-            mineField.openTile(row.intValue(),column.intValue());
+
+            if(mineField.getState()==FieldState.PLAYING) {
+                if (marking) {
+                    mineField.markTile(row.intValue(), column.intValue());
+                } else {
+                    mineField.openTile(row.intValue(), column.intValue());
+                }
+            }
+
         }
         return("mines");
     }
 
-    public String getCurrentTime(){
-        return new Date().toString();
+    @RequestMapping("/mark")
+    public String changeMode(){
+        this.marking=!this.marking;
+        return("mines");
     }
 
+    @RequestMapping("/new")
+    public String newGame(){
+        this.marking=false;
+        this.mineField = new Field(9,9,3);
+        return("mines");
+    }
+
+
+//    public String getCurrentTime(){
+//        return new Date().toString();
+//    }
+
+    public boolean isMarking() {
+        return this.marking;
+    }
     public  String getHtmlField(){
         StringBuilder sb = new StringBuilder();
 
@@ -47,13 +72,13 @@ public class MinesController {
                 Tile tile =mineField.getTile(row,col);
                 sb.append("<td class='"+getTileClass(tile)+"'>");
 
-                if(tile.getState() != TileState.OPEN){
+                if(tile.getState() != TileState.OPEN && mineField.getState()==FieldState.PLAYING){
                     sb.append("<a href='/mines?row="+row+"&column="+col+"'>");
                 }
 
                 sb.append("<span>"+getTileText(tile)+"</span>");
 
-                if(tile.getState() != TileState.OPEN){
+                if(tile.getState() != TileState.OPEN  && mineField.getState()==FieldState.PLAYING){
                     sb.append("</a>");
                 }
                 sb.append("</td>\n");
@@ -62,6 +87,7 @@ public class MinesController {
         }
         sb.append("</table> \n");
         return sb.toString();
+
     }
 
     private String getTileClass(Tile tile) {
@@ -97,6 +123,25 @@ public class MinesController {
                 throw new IllegalArgumentException("Unsupported tile state " + tile.getState());
         }
     }
+
+    public String getGameStatusMessage(){
+        String gameStatus="";
+        FieldState gameState= this.mineField.getState();
+        if(gameState== FieldState.FAILED){
+            gameStatus="You failed.";
+        }else if(gameState== FieldState.SOLVED){
+            gameStatus="You won (score: "+this.mineField.getScore()+")";
+        }else{
+            gameStatus="Playing - ";
+            if(this.marking){
+                gameStatus+="marking";
+            }else{
+                gameStatus+="opening";
+            }
+        }
+        return gameStatus;
+    }
+
 
 
 }
